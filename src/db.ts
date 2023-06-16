38359fbdac9075d02b4f978426643a2181c8a695
OliviaA22 <olivia_adaugo@yahoo.com>
2023-06-16 19:51:23 +0200

    edited connection string

--- src/db.ts
+++ src/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const config = process.env;
 
 const connectToDb = async () => {
-  const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vr5m2af.mongodb.net/`;
+  const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vr5m2af.mongodb.net/<DocMatch>?retryWrites=true&w=majority`;
   
   const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
