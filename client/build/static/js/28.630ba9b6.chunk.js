0ecd2eaea556d2ada7ed8452751c6334db60ac53
OliviaA22 <olivia_adaugo@yahoo.com>
2023-07-14 15:00:00 +0200

    added build dir

---
+++ client/build/static/js/28.630ba9b6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdoc-match"]=this["webpackJsonpdoc-match"]||[]).push([[28],{521:function(t,c,e){"use strict";e.r(c);e(0);var n=e(62),o=e(2);c.default=function(t){var c=t.match;return Object(o.jsx)("button",{onClick:function(){n.a.delete("https://tame-jersey-clam.cyclic.app/api/doctors/".concat(c.params.id)).then((function(t){return console.log(t)}))},children:"Delete Doctor"})}}}]);
+//# sourceMappingURL=28.630ba9b6.chunk.js.map
